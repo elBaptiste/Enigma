4f1d37a09cfa6a09ada9ef21bf18366c572501ab
elBaptiste <villeret.baptiste@gmail.com>
2024-12-20 11:08:06 +0100

    apprentissage gestion de fichiers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
-//
-// Created by bapti on 20/12/2024.
-//
+#include <fstream>
 #include <iostream>
+#include <string>
+
 int main() {
     std::cout << "Hello World!\n";
+
+    std::ifstream fichier { "C:/Users/bapti/CLionProjects/Enigma/texte.txt" };
+
+    if (!fichier.is_open()) {
+        std::cerr << "Erreur : impossible d'ouvrir le fichier texte.txt" << std::endl;
+        return 1;
+    }
+
+    std::string ligne;
+    while (std::getline(fichier, ligne)) {
+        std::cout << ligne << std::endl;
+    }
+
+    return 0;
 }
