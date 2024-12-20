58a56c0ed8031acd90718bf344771a31130bc3dc
augustin <augugu78@gmail.com>
2024-12-20 11:04:20 +0100

    creation de la classe fille enigma pas fini

---
+++ Enigma.cpp
@@ -0,0 +1,5 @@
+//
+// Created by augus on 20/12/2024.
+//
+
+#include "Enigma.h"
---
+++ Enigma.h
@@ -0,0 +1,18 @@
+//
+// Created by augus on 20/12/2024.
+//
+#include <iostream>
+#include "Encrypt.h"
+
+#ifndef ENIGMA_H
+#define ENIGMA_H
+
+
+
+class Enigma : public Encrypt{
+
+};
+
+
+
+#endif //ENIGMA_H
