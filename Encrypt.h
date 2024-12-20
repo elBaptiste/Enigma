9922547989ec3df4f36e686a23bfa16cec536a63
augustin <augugu78@gmail.com>
2024-12-20 11:06:46 +0100

    creation de la classe fille enigma pas fini V2

---
+++ Encrypt.h
@@ -0,0 +1,27 @@
+//
+// Created by augus on 20/12/2024.
+//
+#include <iostream>
+#ifndef ENCRYPT_H
+#define ENCRYPT_H
+
+
+class Encrypt {
+protected:
+     std::string _plain;
+     std:: string _cipher;
+
+
+public :
+     std::string plain();
+     std::string cipher();
+     virtual std::string encod();
+     virtual std::string decod();
+     std::string read();
+     std::string write();
+//test
+};
+
+
+
+#endif //ENCRYPT_H
