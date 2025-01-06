4dbcc11ea51602327951f2f92995e72004cb2e0c
augustin <augugu78@gmail.com>
2025-01-06 10:53:52 +0100

    baptiste a besoin du code

--- Enigma.cpp
+++ Enigma.cpp
@@ -38,6 +38,7 @@ std::string Enigma::encod(std::string message, std::string clef1){
     std::string nouveaumessage ="";
     char charAAjouter = 'a';
     for (int i = 0; i < message.size() ; i++) {
+        if
         nouveaumessage.push_back(clef1[Enigma::getPlaceAlphabet(message[i])]); // ligne pas belle pour dire qu on ajoute a la fin de nouveaumessage le caractere encodé
     }
     return nouveaumessage;
--- main.cpp
+++ main.cpp
@@ -21,12 +21,16 @@ int main() {
     */
 
     std::string clef = Enigma::generateKey();
-    std::cout << clef << std::endl;/*
-    std::string fichier = "C:/Users/bapti/CLionProjects/Enigma/texte.txt" ;
+    std::cout << clef << std::endl;
+    //std::string fichier = "C:/Users/bapti/CLionProjects/Enigma/texte.txt" ;
+    std::string fichier = "C:/Users/augus/CLionProjects/Enigma/texte.txt" ;
     std::string texte_a_lire = read(fichier);
     std::cout << "Contenu du fichier :" << std::endl;
     std::cout << texte_a_lire << std::endl;
-    std::string texte_a_ecrire = write(fichier);*/
+    std::string texte_a_ecrire = write(fichier);
+
+    Enigma enigma = Enigma();
+    std::cout<< enigma.encod(texte_a_lire, clef);
 
     return 0;
 }
